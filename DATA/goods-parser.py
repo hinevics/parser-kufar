2eb469804f246e3f897bfeb83e7f08c67986729a
hinevics <hinevics@gmail.com>
2021-08-16 20:24:25 +0300

    Create goods-parser.py

---
+++ DATA/goods-parser.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import re
+import os.path
+import hashlib
+
+
+def parser():
+    pass
+
+
+def main():
+    parser()
+
+
+if __name__ == '__main__':
+    main()
