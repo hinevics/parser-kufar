40ccaf2e8015f6efefbf2444f042895e95ec08c4
hinevics <hinevics@gmail.com>
2021-08-17 22:06:34 +0300

    Update goods-parser.py

--- goods-parser.py
+++ goods-parser.py
@@ -12,6 +12,45 @@ DEFAULT_NAME_DIRECTORY_DB = r"directory_link.json"
 DEFAULT_NAME_SUBDIRECTORY_DB = r"subdirectory_link.json"
 
 
+# links to pages in this category
+NUMBER_LINKS_PAGES_CATALOG = {}
+# products from pages
+GOODS = []
+
+
+def collection_ads(soup_obj: BeautifulSoup):
+    article = soup_obj.find('article')
+    a = article.find_all('a', {'target': '_blank'})
+    for i in range(len(a)):
+        link_product = a[i].get('href')
+        photo_link = a[i].find('img').get('data-src')  # link in the photo
+        name_product = a[i].find('img').get('alt')
+        data_time = parser_data_and_time(a[i].find('span').text)
+        info1 = a[i].find('div').find_next_sibling()
+        description = info1.find('h3').text
+        info2 = info1.find('div').find_next_sibling()
+        price = info2.find('div').find('span').text
+        location = info2.find('div').find_next_sibling().find('span').text
+        GOODS.append(dict(
+            link_product=link_product,
+            photo_link=photo_link,
+            name_product=name_product,
+            data_time=data_time,
+            description=description,
+            price=price,
+            location=location))
+
+
+def search_link_page(soup_obj: BeautifulSoup):
+    """
+    Search for links to other catalog pages
+    """
+    numbers_webpages = soup_obj.find('div', {'data-name':'listings-pagination'}).find_all('a')
+    # Get page numbers and links
+    for i in numbers_webpages:
+        NUMBER_LINKS_PAGES_CATALOG[i.text] = 'https://www.kufar.by/' + i.get('href')
+
+
 def beautifulSoup_object_creation(url: str, header:dict):
     """
     description:
@@ -38,8 +77,9 @@ def parser(path:str=DEFAULT_PATH_DB, name:str=DEFAULT_NAME_SUBDIRECTORY_DB, d_ur
     # for link in reading_links_subdirectories(path=DEFAULT_NAME_DIRECTORY_DB, name=DEFAULT_NAME_SUBDIRECTORY_DB)
     for link in reading_links_subdirectories(path=path, name=name):
         soup = beautifulSoup_object_creation(url='{d_url}/{link}'.format(d_url=d_url, link=link), header=header)
-        print(soup)
-
+        search_link_page(soup_obj=soup)
+        max_page = int(max(NUMBER_LINKS_PAGES_CATALOG.keys()))
+        break
 
 def main():
     parser()
